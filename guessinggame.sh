b628c057a1b245d90356de5b8cc979bcabe30a0f
kush840garg <62303189+kush840garg@users.noreply.github.com>
2020-10-13 17:01:45 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,15 @@
 echo "Welcome to Guessing game"
-
 function input {
-
 	echo "Enter the number of files in the current directory:"
-  read guess
-  
+  read guess  
   f=$(ls -1 | wc -l)
+  if[[ ! $guess =~ ^[0-9]+$ ]]
+  then 
+  echo "Invalid input enter a number"
+  exit
+  fi
 }
-
 input
-
 while [[ $guess -ne $f ]]
 do
 	if [[ $guess -lt $f ]] 
